wikimedia/mediawiki-extensions-Cargo

Reorder files and directories for consistency
Change-Id: I75583d7415039f8021646de52b2206da50b08139

libs/ext.cargo.recreatedata.js
@@ -39,7 +39,7 @@
 	 */
 	recreateData.createJobs = function( templateNum, numPagesHandled, replaceOldRows ) {
 		var curTemplate = templateData[templateNum];
-		var progressImage = "<img src=\"" + cargoScriptPath + "/skins/loading.gif\" />";
+		var progressImage = "<img src=\"" + cargoScriptPath + "/resources/images/loading.gif\" />";
 		if ( numTotalPages > 1000 ) {
 			var remainingPixels = 100 * numTotalPagesHandled / numTotalPages;
 			progressImage = "<progress value=\"" + remainingPixels + "\" max=\"100\"></progress>";
@@ -84,7 +84,7 @@
 		recreateData.replaceForm();
 
 		if ( isDeclared ) {
-			$("#recreateTableProgress").html( "<img src=\"" + cargoScriptPath + "/skins/loading.gif\" />" );
+			$("#recreateTableProgress").html( "<img src=\"" + cargoScriptPath + "/resources/images/loading.gif\" />" );
 			var queryStringData = {
 				action: "cargorecreatetables",
 				template: templateData[0].name,

Wikia/Cargo

libs/ext.cargo.recreatedata.js
@@ -37,7 +37,7 @@
 	 */
 	recreateData.createJobs = function( templateNum, numPagesHandled, replaceOldRows ) {
 		var curTemplate = templateData[templateNum];
-		var progressImage = "<img src=\"" + cargoScriptPath + "/skins/loading.gif\" />";
+		var progressImage = "<img src=\"" + cargoScriptPath + "/resources/images/loading.gif\" />";
 		if ( numTotalPages > 1000 ) {
 			var remainingPixels = 100 * numTotalPagesHandled / numTotalPages;
 			progressImage = "<progress value=\"" + remainingPixels + "\" max=\"100\"></progress>";
@@ -81,7 +81,7 @@
 		recreateData.replaceForm();
 
 		if ( isDeclared ) {
-			$("#recreateTableProgress").html( "<img src=\"" + cargoScriptPath + "/skins/loading.gif\" />" );
+			$("#recreateTableProgress").html( "<img src=\"" + cargoScriptPath + "/resources/skins/loading.gif\" />" );
 			var api = new mw.Api();
 			api.postWithToken(
 				'csrf',